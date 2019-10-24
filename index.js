b0f643608260d1742ca0ca2b511234e451c09913
Hamza Ali <hamzali.tas.95@gmail.com>
2019-10-24 15:32:40 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+var aedes = require('aedes')()
+var server = require('net').createServer(aedes.handle)
+var port = 1883
+
+server.listen(port, function () {
+  console.log('server listening on port', port)
+})
+
+server.on('client', (client) => {
+    console.log("client", client);
+
+})
